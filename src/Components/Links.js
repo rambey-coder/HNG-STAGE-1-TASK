91ce7728f8dbbcfc8446a59e5c336c11c3eb93d1
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-23 14:52:56 +0100

    link added

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -11,13 +11,13 @@ const MainPage = () => {
         <a href="https://twitter.com/Rambeybello">Twitter Link</a>
         <a href="https://training.zuri.team/">Zuri Team</a>
         <a href="http://books.zuri.team">Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>">Python Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=<rambey>">Python Books</a>
         <a href="https://background.zuri.team">Background Checker for Coders</a>
         <a href="https://books.zuri.team/design-rules ">Design Books</a>
       </div>
 
       <div className="account">
-        <a href="rambeybello" id='slack'>
+        <a href="https://rambey.slack.com" id='slack'>
           <img src="/assets/slack-icon.svg" alt="slack" />
         </a>
 
