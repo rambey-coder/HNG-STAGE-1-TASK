fc6a51745e357ffd00d230c5626342f758cfa292
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-23 12:48:43 +0100

    designing error component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './Components/Header';
 import Links from './Components/Links'
 import Footer from './Components/Footer';
+import Error from './Components/Error';
 
 function App() {
   return (
      <div className="container">
-      <Header />
+      {/* <Header />
       <Links />
-      <Footer />
+      <Footer /> */}
+      <Error />
      </div>
   );
 }
---
+++ src/Components/Error.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Error = () => {
+  return (
+    <>
+    <div className="error">
+        <p>404 error</p>
+        <h1>We can’t find that page</h1>
+        <p>Sorry, the page you are looking for doesn't exist.</p>
+        <div className="error-link">
+            <a href="">
+                <img src="/assets/back-icon.svg" alt="back" />
+                Go back
+            </a>
+
+            <a href="">Take me home</a>
+        </div>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default Error
