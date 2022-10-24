36379b51665cfdeaba8844147a9b59c49176957a
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-24 13:32:39 +0100

    profile picture and tooltip

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,13 +3,17 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
-      <span>
+      <div>
+        <div className='profile-container'>
         <img src="/assets/profile__img.png" alt="profile_img" id="profile_img" />
+        <img src="/assets/camera-icon.svg" className='camera' alt="camera" />
+        </div>
         <p id='twitter'>Rambeybello</p>
-      </span>
-        <span>
+      </div>
+        <span className='share-tooltip'>
           <img src="/assets/share-icon.svg" alt="share" className='share'/>
           <img src="/assets/menu-icon.svg" alt="menu" className='menu'/>
+          <p className='tiptext'>Share Link</p>
         </span>
       </div>
   )
