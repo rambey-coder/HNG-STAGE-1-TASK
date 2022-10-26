54afd099489f12e052ae1bbfff656f868f3d1a36
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-26 10:12:47 +0100

    completed the task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Links from './Components/Links'
-import Error from './Components/Error';
+// import Error from './Components/Error';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
      <div className="container">
     <Routes>
       <Route path='/' element={<Links />} />
-      <Route path='*' element={<Error />} />
+      {/* <Route path='*' element={<Error />} /> */}
      </Routes>
      </div>
      </Router>
--- src/Components/Links.js
+++ src/Components/Links.js
@@ -9,21 +9,17 @@ const MainPage = () => {
 
     <div className="links">
         <a href="https://twitter.com/Rambeybello">Twitter Link</a>
-        <a href="https://training.zuri.team/">Zuri Team</a>
-        <a href="http://books.zuri.team">Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=<rambey>">Python Books</a>
-        <a href="https://background.zuri.team">Background Checker for Coders</a>
-        <a href="https://books.zuri.team/design-rules ">Design Books</a>
+        <a href="https://training.zuri.team/" id='btn__zuri'>Zuri Team</a>
+        <a href="http://books.zuri.team" id='books'>Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=<rambey>" id='book__python'>Python Books</a>
+        <a href="https://background.zuri.team" id='pitch'>Background Checker for Coders</a>
+        <a href="https://books.zuri.team/design-rules" id='book__design'>Design Books</a>
       </div>
 
       <div className="account">
-        <a href="https://rambey.slack.com" id='slack'>
           <img src="/assets/slack-icon.svg" alt="slack" />
-        </a>
 
-        <a href="#!https://github.com/rambey-coder" id='github'>
           <img src="/assets/github-icon.svg" alt="github" />
-        </a>
       </div>
 
       <Footer />
